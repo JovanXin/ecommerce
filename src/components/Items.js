9831ae85c822f28ef69364ae079f2958304fec0c
JovanXin <jovan3.1415926@gmail.com>
2021-02-08 12:36:30 +1300

    Actually fix key error by remembering _id instead of id

--- src/components/Items.js
+++ src/components/Items.js
@@ -4,12 +4,14 @@ import Item from "./Item.js"
 export default function Items(props) {
     const { itemsData } = props;
 
+    console.log(itemsData)
+
     return (
         <div className="container">
             <ul className="items">
-            {itemsData.map((itemData => (
-                <li key={itemData._id}>
-                    <Item name={itemData.name} price={itemData.price} description={itemData.description} />
+            {itemsData.map((itemsData => (
+                <li key={itemsData._id}>
+                    <Item name={itemsData.name} price={itemsData.price} description={itemsData.description} />
                 </li>
             )))}
             </ul>
