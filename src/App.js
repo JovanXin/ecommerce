9e9467349da0f36b8fd175518a80eec3073ad11b
JovanXin <jovan3.1415926@gmail.com>
2021-02-08 13:38:38 +1300

    Move nav component out of header and into app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+
+import Nav from "./components/Nav"
 import Switcher from "./Switcher";
 
 export default function App() { //What is the purpose of App if I only import switcher?
     return (
         <div className="app">
+            <Nav />
             <Switcher />
         </div>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Nav from "./Nav";
 
 export default function Header(props) {
     return(
         <div>
-            <Nav />
             <h1>{props.name}</h1>
         </div>
     )
