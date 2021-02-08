00563e015859713a8426eeab95ad81d17a2b3274
JovanXin <jovan3.1415926@gmail.com>
2021-02-08 14:06:14 +1300

    Create seperate frontend and backend folders

---
+++ frontend/src/pages/Item.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function ItemPage() { // Show image, detailed description, price, purchase options
+    return (
+        <div>
+        </div>
+    )
+}
